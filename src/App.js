cc9020a78fd587124f29b1b15390287ec36842ff
Mark <mark.kimk622@gmail.com>
2022-04-06 02:01:23 -0400

    organized components and added search bar

--- src/App.js
+++ src/App.js
@@ -5,98 +5,70 @@ import axios from 'axios';
 import './App.css';
 // Components
 import Header from './components/Header.js';
+import PokemonList from './components/PokemonList';
+import SinglePokemon from './components/SinglePokemon';
 
 function App() {
-  //  create variables that:
-  // 	  1 holds api url for searching for a list of pokemon
-  // 	  2 holds search parameters for offset and limit
-  // 	  3 hold the variable for offset
-  // 		    starts at 0
-  // 	    limit will be set to 6, no need for a seperate variable 
 
   const [apiOffSet, setApiOffSet] = useState(0);
   const [apiLimit, setApiLimit] = useState(6);
-  const [pokemonList, setPokemonList] = useState([]);
-  const [pokemonArray, setPokemonArray] = useState([]);
-
-
+  const [userInput, setUserInput] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
   const apiUrl = `https://pokeapi.co/api/v2/pokemon/`
 
-  // create a function (getListOfPokemon) to retreive data from the api and THEN call the getIndividualPokemon function
-  useEffect(() => {
-    axios({
-      url: apiUrl,
-      method: "GET",
-      dataResponse: "json",
-      params: {
-        offset: apiOffSet,
-        limit: apiLimit,
-      },
-    }).then((response) => {
-      setPokemonList(response.data.results);
-      console.log(response.data.results);
-    }).catch((error) => {
-      console.log(error);
-    })
-  }, [apiOffSet])
 
-
-  // create a function (getIndividualPokemon) to retreieve the data from each item in the array
-  // 	  another fetch function may be needed and THEN displayPokemon
-
-  useEffect(() => {
-    const pokePromises = pokemonList.map((pokemonFromList) => {
-      return axios({
-        url: pokemonFromList.url,
-        method: "GET",
-        dataResponse: "json",
-      })
-    })
-    Promise.all(pokePromises).then((response) => {
-      setPokemonArray(response)
-    })
-  }, [pokemonList])
-
-  console.log(pokemonArray);
-
-
-
-  // create a function (changePokemon) to change the variable that holds the offset variable for the api search parameter when the backward and forward buttons are clicked
-  // 	when the forward button is clicked, add 6 to the variable,
-  // 	when the backward button is clicked, subtract 6 of the variable
-  // 	put apiOffSet in the dependancy array for the first useEffect so that when the apiOffset variable is changed, the useEffect is triggered again.
   const handleForward = (number) => {
     number = apiOffSet + 6;
     setApiOffSet(number);
-  }
+  };
 
   const handleBackward = (number) => {
     number = apiOffSet - 6;
     setApiOffSet(number);
+  };
+
+  const handleInput = (e) => {
+    setUserInput(e.target.value)
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchTerm(userInput)
   }
+
+
 
 
   return (
     <div className="App">
       <Header />
 
-
-      {/* // create a function (displayPokemon) that will display the name and image of each pokemon onto the page */}
       <main>
         <section className='wrapper'>
           <div className='buttonFlex'>
             <button onClick={handleBackward}>backward</button>
+
             <button onClick={handleForward}>forward</button>
           </div>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="search">Search for a Pokemon</label>
+            <input type="text" id="search" onChange={handleInput} value={userInput} placeholder="Enter a name or number" />
+            <button>Search</button>
+          </form>
           <ul className='pokemonResults'>
-            {pokemonArray.map((pokemon) => {
-              return (
-                <li key={pokemon.data.id} className='pokemonCard'>
-                  <h2>{pokemon.data.name}</h2>
-                  <img src={pokemon.data.sprites.front_default} alt={`${pokemon.data.name}`} /> 
-                </li>
-              )
-            })}
+            {!searchTerm 
+            ?
+              <PokemonList
+                apiOffSet={apiOffSet}
+                apiLimit={apiLimit}
+                apiUrl={apiUrl}
+              />
+              :
+              <SinglePokemon
+                searchTerm={searchTerm}
+                apiUrl={apiUrl}
+              />
+            }
           </ul>
           <div className='buttonFlex'>
             <button onClick={handleBackward}>backward</button>
---
+++ src/components/SinglePokemon.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import '../App.css';
+
+const SinglePokemon = (props) => {
+    const [singlePokemon, setSinglePokemon] = useState();
+    const { apiUrl, searchTerm } = props;
+
+    useEffect(() => {
+        axios({
+            url: apiUrl + searchTerm,
+            method: "GET",
+            dataResponse: "json",
+        }).then((response) => {
+            console.log(response.data);
+            setSinglePokemon(response.data)
+
+        }).catch((error) => {
+            console.log(error);
+        })
+    }, [searchTerm])
+
+    return (
+        <>
+
+            {singlePokemon ?
+                <li key={singlePokemon.id} className='pokemonCard'>
+                    <h2>{singlePokemon.name}</h2>
+                    <img src={singlePokemon.sprites.front_default} alt={`${singlePokemon.name}`} />
+                </li>
+                :
+                <h2>Loading...</h2>
+            }
+
+        </>
+    )
+}
+
+export default SinglePokemon;
